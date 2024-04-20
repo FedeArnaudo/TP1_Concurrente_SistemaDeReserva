9ec273a40694b45eb1536be8768bbf77666167a8
NahuelAroca <117319043+NahuelAroca@users.noreply.github.com>
2024-04-20 12:24:10 -0300

    Update ProcesoDeVerificacion.java

--- src/ProcesoDeVerificacion.java
+++ src/ProcesoDeVerificacion.java
@@ -6,6 +6,16 @@ public class ProcesoDeVerificacion implements Runnable{
 
     @Override
     public void run() {
-
+        int cantidadReservas = 0;
+        while(cantidadReservas != vuelo.getMatrizDeAsientos().getCANTIDAD_MAX_ASIENTOS() || vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() > 0   ){
+            cantidadReservas = vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CANCELADAS).getSize() + vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() + vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.VERIFICADAS).getSize();
+            Reserva reserva = vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getReserva();
+            if(reserva == null) continue;
+            if(reserva.getAsiento().getEstado().equals(ESTADO.VALIDADO)){
+                if(vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).deleteReserva(reserva)) {
+                    vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.VERIFICADAS).addReserva(reserva);
+                }
+            }
+        }
     }
 }
