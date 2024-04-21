7ff936cf24c1015fb13646f6f692288377ba1f6b
NahuelAroca <117319043+NahuelAroca@users.noreply.github.com>
2024-04-21 10:57:26 -0300

    Update ProcesoDeVerificacion.java

--- src/ProcesoDeVerificacion.java
+++ src/ProcesoDeVerificacion.java
@@ -6,16 +6,22 @@ public class ProcesoDeVerificacion implements Runnable{
 
     @Override
     public void run() {
+
         int cantidadReservas = 0;
-        while(cantidadReservas != vuelo.getMatrizDeAsientos().getCANTIDAD_MAX_ASIENTOS() || vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() > 0){
-            cantidadReservas = vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CANCELADAS).getSize() + vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() + vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.VERIFICADAS).getSize();
-            Reserva reserva = vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).getReserva();
+        while(cantidadReservas != vuelo.getMatrizDeAsientos().getCANTIDAD_MAX_ASIENTOS() || vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() > 0){
+            cantidadReservas = cantReservas();
+            Reserva reserva = vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.CONFIRMADAS).getReserva();
             if(reserva == null) continue;
-            if(checked(reserva)){
-                if(vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.CONFIRMADAS).deleteReserva(reserva)) {
-                    vuelo.getRegistros().getReservas(TIPO_DE_RESERVA.VERIFICADAS).addReserva(reserva);
-                }
+            if(reserva.getAsiento().getEstado().equals(ESTADO.CHECKED)){
+                verificar(reserva);
             }
         }
+    }
+    public void verificar(Reserva reserva){
+        vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.CONFIRMADAS).deleteReserva(reserva);
+        vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.VERIFICADAS).addReserva(reserva);
+    }
+    public int cantReservas(){
+        return vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.CANCELADAS).getSize() + vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.CONFIRMADAS).getSize() + vuelo.getRegistroDeReservas().getBufferDeReservas(TIPO_DE_RESERVA.VERIFICADAS).getSize();
     }
 }
